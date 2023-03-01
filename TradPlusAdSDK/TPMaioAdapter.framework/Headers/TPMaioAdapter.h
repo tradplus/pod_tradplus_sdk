88c0975da78ac6dfa93a8673cdb0a5ffd672bb6e
xuejun <xuejun.pei@tradplus.com>
2023-03-01 15:24:14 +0800

    update

---
+++ TradPlusAdSDK/TPMaioAdapter.framework/Headers/TPMaioAdapter.h
@@ -0,0 +1,4 @@
+#import <MaioAdapter/TradPlusMaioInterstitialAdapter.h>
+#import <MaioAdapter/TradPlusMaioRewardedAdapter.h>
+#import <MaioAdapter/TradPlusMaioSDKLoader.h>
+#import <MaioAdapter/TPMaioAdapterBaseInfo.h>
